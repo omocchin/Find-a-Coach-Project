29a4369a29541fd0cfaa2e7aba9964ee71c19f5e
Kyle Mitsui <omocchin@gmail.com>
2023-11-25 10:42:34 +0900

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-module.exports = {
-  outputDir: 'docs',
-  assetsDir: './',
-  publicPath: './'
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-deploy/" : "/",
+})
