6843fb0aa23a2a28a68bc0ccc5bf1e6a6943ce49
Kyle Mitsui <omocchin@gmail.com>
2023-11-26 23:14:13 +0900

    netlify deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/find-a-coach-project/'
-    : '/'
+  pwa: {
+    workboxOptions: {
+      exclude: [/_redirects/]
+    }
+  }
 }
