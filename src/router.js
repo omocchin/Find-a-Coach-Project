5466594a44d33d58ae9a31221e57b41842d5bdde
Kyle Mitsui <omocchin@gmail.com>
2023-11-26 23:31:16 +0900

    fix

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter } from 'vue-router'
 
 // import CoachDetail from './pages/coaches/CoachDetail.vue'
 import CoachesList from './pages/coaches/CoachesList.vue'
@@ -19,7 +19,6 @@ const UserAuth = import('./pages/auth/UserAuth.vue')
 
 
 const router = createRouter({
-  history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/coaches'},
     { path: '/coaches', component: CoachesList },
