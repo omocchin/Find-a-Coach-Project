7bcf2905b1eac72609ae4fa4461d8357a786b4d9
Kyle Mitsui <omocchin@gmail.com>
2023-11-27 22:32:49 +0900

    final fix?

--- src/router.js
+++ src/router.js
@@ -1,22 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
-// import CoachDetail from './pages/coaches/CoachDetail.vue'
 import CoachesList from './pages/coaches/CoachesList.vue'
-// import CoachRegisteration from './pages/coaches/CoachRegisteration.vue'
-// import ContactCoach from './pages/requests/ContactCoach.vue'
-// import RequestReceived from './pages/requests/RequestsReceived.vue'
 import NotFound from './pages/NotFound.vue'
-// import UserAuth from './pages/auth/UserAuth.vue'
 import store from './store/index.js'
-
-const CoachDetail = import('./pages/coaches/CoachDetail.vue')
-const CoachRegisteration = import('./pages/coaches/CoachRegisteration.vue')
-const ContactCoach = import('./pages/requests/ContactCoach.vue')
-const RequestReceived = import('./pages/requests/RequestsReceived.vue')
-const UserAuth = import('./pages/auth/UserAuth.vue')
-
-
-
+import CoachDetail from './pages/coaches/CoachDetail.vue'
+import CoachRegisteration from './pages/coaches/CoachRegisteration.vue'
+import ContactCoach from './pages/requests/ContactCoach.vue'
+import RequestReceived from './pages/requests/RequestsReceived.vue'
+import UserAuth from './pages/auth/UserAuth.vue'
 
 const router = createRouter({
   history: createWebHistory(),
